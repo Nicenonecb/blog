59bd25e9d91fea73450ccb6c56cb17df24fcbfae
wbz <1664546556@qq.com>
2024-01-05 23:11:19 +0800

    feat: 评论系统引入

---
+++ app/components/UtterancesComments.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+const UtterancesComments = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://utteranc.es/client.js";
+        script.async = true;
+        script.setAttribute("repo", "blog");
+        script.setAttribute("issue-term", `https://blog-three-green-37.vercel.app/posts/${id}`);
+        script.setAttribute("theme", "github-light");
+        script.setAttribute("crossorigin", "anonymous");
+
+        const scriptParentNode = document.getElementById("comments");
+        scriptParentNode.appendChild(script);
+
+        return () => {
+            scriptParentNode.removeChild(scriptParentNode.firstChild);
+        };
+    }, []);
+
+    return <div id="comments"></div>;
+};
+
+export default UtterancesComments;
